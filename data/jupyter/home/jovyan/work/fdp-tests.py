2895b9d6c94fed4a0e6a2784df4faa44d6584082
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-04-28 16:46:45 +0200

    further implement fdpclient and tests

--- data/jupyter/home/jovyan/work/fdp-tests.py
+++ data/jupyter/home/jovyan/work/fdp-tests.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fdpclient.fdpclient import FDPClient
 import fdpclient
 from importlib import reload
@@ -5,19 +7,86 @@ reload(fdpclient.fdpclient)
 import logging
 import requests
 from requests.auth import HTTPBasicAuth
-
+import rdflib
+from pprint import pprint
+import datatime
 
 #my_headers = {'Accept' : 'application/json','Content-Type':'application/json'}
-client=FDPClient("http://localhost:8080","albert.einstein@example.com","password")
+client=FDPClient("http://localhost:8080","albert.einstein@example.com","password",publicurl="http://localhost:8080")
 client.headers
+client.catalog_template
+
+pprint(client.read().serialize(format="turtle"))
+pprint(client.read(type="meta").serialize(format="turtle"))
+
+newcat=client.createCatalogRDF(DESCRIPTION="test",title="Test02",version="1.0.0",ispartof=client.publicurl,publisher="ICH")
+id=client.create(type='catalog',data=newcat)
+
+newdat=client.createDatasetRDF(title="COVAC-DM Study",
+                               version="1.0.0",
+                               catalogid=id,
+                               publisher="Projektmanagementteam",
+                               ISSUED=datetime.datetime.now(),
+                               MODIFIED=datetime.datetime.now(),
+                               LANGUAGE='http://id.loc.gov/vocabulary/iso639-1/en',
+                               KEYWORDS=["diabetes mellitus type 1","diabetes mellitus type 2","COVID-19"],
+                               DESCRIPTION="<https://biobank.medunigraz.at/.../biobank/pdf/Kohorten/5006_21_COVAC-DM_Study.pdf>",
+                               CONTACTPOINT = "patrick.nitsche@medunigraz.at",
+                               themes_list=["http://www.w3.org/ns/dcat#theme","http://identifiers.org/icd/Q87.8"])
+dat_id=client.create(type='dataset',data=newdat)
+
+newdis=client.createDistributionRDF(title="Distribution",
+                               version="1.0.0",
+                               datasetid=dat_id,
+                               publisher="Projektmanagementteam",
+                               ISSUED=datetime.datetime.now(),
+                               MODIFIED=datetime.datetime.now(),
+                               LANGUAGE='http://id.loc.gov/vocabulary/iso639-1/en',
+                               mediatype="WSI",
+                               FORMAT="SVS",
+                               BYTESIZE="10737418240",
+                               ACCESSURL="https://youtu.be/dQw4w9WgXcQ",
+                               DOWNLOADURL="http://download.url")
+dis_id=client.create(type='distribution',data=newdis)
 
 # create metadata
 with open('test-data/test_cat02.ttl') as f:
     data = f.read()
 
 id=client.create(type='catalog',data=data)
-client.read(type='catalog',id=id)
+cat_meta=client.read(type='catalog',id=id)
+for r in cat_meta.triples((None, None, None)):
+    print(r)
+
+#(rdflib.term.URIRef('http://localhost/catalog/4f6f1e29-a76b-4d7d-9441-f28dcabb3475'), rdflib.term.URIRef('http://purl.org/dc/terms/hasVersion'), rdflib.term.Literal('1'))
+
+qres = cat_meta.query("SELECT ?s ?o WHERE { ?s dcterms:hasVersion ?o }")
+for row in qres:
+    print(f"{row.s} dcterms:hasVersion {row.o}")
+
+cat_meta.update("""
+         DELETE { ?s dcterms:hasVersion '1' }
+         INSERT { ?s dcterms:hasVersion '2' }
+         WHERE { ?s dcterms:hasVersion '1' }
+         """)
+
+qres = cat_meta.query("SELECT ?s ?o WHERE { ?s dcterms:hasVersion ?o }")
+for row in qres:
+    print(f"{row.s} dcterms:hasVersion {row.o}")
+
+cat_meta.update("""
+             INSERT DATA { <http://localhost/catalog/4f6f1e29-a76b-4d7d-9441-f28dcabb3475> dcterms:hasVersion <2> }
+             """)
+cat_meta.update("""
+         DELETE { ?s dcterms:hasVersion <2> }
+         WHERE { ?s dcterms:hasVersion <2> }
+         """)
+
+
+
+client.update(type='catalog', id=id, data=cat_meta.serialize(format="turtle"))
 client.delete(type='catalog',id=id)
+
 id='b01c85a9-aad5-45e7-9ec7-d6a59437de37'
 
 client.delete(type='catalog',id='815e5aae-44e9-4f16-a76c-f395b6e82ab5')
@@ -28,3 +97,35 @@ client.delete(type='catalog',id='815e5aae-44e9-4f16-a76c-f395b6e82ab5')
 #r=requests.post("http://localhost:8080/tokens", data=json.dumps(test_data),headers=my_headers)
 #r.json()['token']
 #requests.post("http://localhost:8080/tokens", auth=HTTPBasicAuth('albert.einstein@example.com', 'password'),headers=my_headers).content
+
+
+
+# http://localhost:8088/catalog/d3a7cf95-fef9-44a0-be0b-6cd0e6385712/create-dataset
+
+
+g = rdflib.Graph()
+g.parse(
+    data="""
+        <x:> a <c:> .
+        <y:> a <c:> .
+    """,
+    format="turtle"
+)
+
+# Select all the things (s) that are of type (rdf:type) c:
+qres = g.query("""SELECT ?s WHERE { ?s a <c:> }""")
+
+for row in qres:
+    print(f"{row.s}")
+
+g.update("""INSERT DATA { <z:> a <c:> }""")
+
+g.update("""
+         DELETE { <y:> a <c:> }
+         INSERT { <y:> a <d:> }
+         WHERE { <y:> a <c:> }
+         """)
+print("After second update:")
+qres = g.query("""SELECT ?s ?o WHERE { ?s a ?o }""")
+for row in qres:
+    print(f"{row.s} a {row.o}")
--- data/jupyter/home/jovyan/work/fdpclient/fdpclient.py
+++ data/jupyter/home/jovyan/work/fdpclient/fdpclient.py
@@ -2,6 +2,7 @@ import rdflib
 import requests
 import logging
 import json
+import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -15,11 +16,22 @@ class FDPClient:
     }
 
 
-    def __init__(self, baseurl, email, password):
+    def __init__(self, baseurl, email, password,publicurl='',
+                 catalog_template='./test-data/catalog_template.ttl',
+                 dataset_template='./test-data/dataset_template.ttl',
+                 distribution_template='./test-data/distribution_template.ttl'
+                 ):
         self.baseurl = baseurl.rstrip('/')
+        if publicurl is not None and publicurl != "":
+            self.publicurl = publicurl.rstrip('/')
+        else:
+            self.publicurl = self.baseurl
         self.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
         self.headers['Authorization'] = f'Bearer {self.gettoken(email, password)}'
         del self.headers['Accept']
+        self.catalog_template = self._load_template(catalog_template)
+        self.dataset_template = self._load_template(dataset_template)
+        self.distribution_template = self._load_template(distribution_template)
 
 
     def gettoken(self, email, password):
@@ -49,13 +61,13 @@ class FDPClient:
             **kwargs: Optional arguments that :func:`requests.request` takes.
         """
         logger.debug(f'Create metadata on {self.baseurl}/{type} with the content: \n{data}')
-        create_headers=self.headers
+        create_headers=self.headers.copy()
         create_headers.update({'Content-Type': self.DATA_FORMATS[format]})
         #print(create_headers)
         try:
             data = self._check_data(data, format)
             r = requests.post(f'{self.baseurl}/{type}', data=data, headers=create_headers)
-            return r.headers['Location'].lstrip(f'{self.baseurl}/{type}/')
+            return r.headers['Location'].lstrip(f'{self.publicurl}/{type}/')
         except Exception as error:
             print(f'Unexpected error when connecting to {self.baseurl}/{type}\n')
             raise error
@@ -65,7 +77,7 @@ class FDPClient:
                       f'\nResponse message: {r.text}')
                 raise
 
-    def read(self, type, id, format='turtle', **kwargs):
+    def read(self, type='', id='' , subtype='',format='turtle', **kwargs):
         """Send a read request.
         Args:
             url(str): URL for reading a metadata.
@@ -77,18 +89,26 @@ class FDPClient:
         Returns:
             :class:`rdflib.Graph`:  RDF graph of the requested metadata.
         """
-        logger.debug(f'Read metadata: {self.baseurl}/{type}/{id}')
+        url = f'{self.baseurl}'
+        if type is not None and type != "":
+            url = f'{url}/{type}'
+            if id is not None and id != "":
+                url = f'{url}/{id}'
+            if subtype is not None and subtype != "":
+                url = f'{url}/{subtype}'
+
+        logger.debug(f'Read metadata: {url}')
         read_headers = self.headers.copy()
         read_headers.update({'Content-Type': self.DATA_FORMATS[format]})
 
         try:
-            r = requests.get(f'{self.baseurl}/{type}/{id}', headers=read_headers, **kwargs)
+            r = requests.get(url, headers=read_headers, **kwargs)
         except Exception as error:
-            print(f'Unexpected error when connecting to {self.baseurl}/{type}/{id}\n')
+            print(f'Unexpected error when connecting to {url}\n')
             raise error
         else:
             if r.status_code != 200:
-                print(f'HTTP error: {r.status_code} {r.reason} for {self.baseurl}/{type}/{id}',
+                print(f'HTTP error: {r.status_code} {r.reason} for {url}',
                       f'\nResponse message: {r.text}')
                 raise
 
@@ -117,11 +137,145 @@ class FDPClient:
                       f'\nResponse message: {r.text}')
                 raise
 
+    def update(self,type='', id='' , subtype='', data="", format='turtle', **kwargs):
+        """Send an update request.
+        Args:
+            url(str): URL for updating a metadata.
+            data(str, bytes, file-like object or :class:`rdflib.Graph`):
+                the content of metadata to send in the request body.
+            format (str, optional): the format of the metadata.
+                This argument overwrites the request header ``content-type``.
+                Available options are 'turtle', 'n3', 'nt', 'xml' and 'json-ld'.
+                Defaults to 'turtle'.
+            **kwargs: Optional arguments that :func:`requests.request` takes.
+        """
+        url = f'{self.baseurl}'
+        if type is not None and type != "":
+            url = f'{url}/{type}'
+            if id is not None and id != "":
+                url = f'{url}/{id}'
+            if subtype is not None and subtype != "":
+                url = f'{url}/{subtype}'
+
+        logger.debug(f'Update metadata on {url} with the content: \n{data}')
+        update_headers = self.headers.copy()
+        update_headers.update({'Content-Type': self.DATA_FORMATS[format]})
+
+        try:
+            data = self._check_data(data, format)
+            r = requests.put(f'{url}', data=data, headers=update_headers, **kwargs)
+            #print(f'{r.status_code} {r.reason}')
+        except Exception as error:
+            print(f'Unexpected error when connecting to {url}\n')
+            raise error
+        else:
+            if r.status_code >= 300:
+                print(f'HTTP error: {r.status_code} {r.reason} for {url}',
+                      f'\nResponse message: {r.text}')
+                raise
+
     def _check_data(self, data, format):
         """Check input data type and convert Graph data to bytes"""
         if isinstance(data, rdflib.Graph):
             return data.serialize(format=format)
         else:
             return data
+
+    def _load_template(self, path):
+        with open(path) as f:
+            data = f.read()
+
+        return data.replace("§§BASEURL",self.publicurl)
+        #graph = rdflib.Graph()
+        #graph.parse(path, format='ttl')
+        #qres = graph.query("SELECT ?s ?o WHERE { ?s dcterms:hasVersion ?o }")
+        #for row in qres:
+        #    print(f"{row.s} dcterms:hasVersion {row.o}")
+
+    def createCatalogRDF(self,title,version,publisher,ispartof=None,**kwargs):
+        catalog = self.catalog_template
+        catalog = catalog.replace(f'§§TITLE', str(title))
+        if ispartof is None:
+            catalog = catalog.replace(f'§§ISPARTOF', str(self.publicurl))
+        else:
+            catalog = catalog.replace(f'§§ISPARTOF', str(ispartof))
+        catalog = catalog.replace(f'§§VERSION', str(version))
+        catalog = catalog.replace(f'§§PUBLISHER', str(publisher))
+
+        for key, value in kwargs.items():
+            catalog = catalog.replace(f'§§{key}',str(value))
+
+        result_catalog=""
+        for line in catalog.split('\n'):
+            if line.find('§§')>0:
+                continue
+            result_catalog=f'{result_catalog}\n{line}'
+        result_catalog=result_catalog.strip()
+        return result_catalog
+
+    def createDatasetRDF(self, title, catalogid, version, publisher, themes_list, **kwargs):
+        dataset = self.dataset_template
+        dataset = dataset.replace(f'§§TITLE', str(title))
+        dataset = dataset.replace(f'§§CATALOGID', str(catalogid))
+        dataset = dataset.replace(f'§§VERSION', str(version))
+        dataset = dataset.replace(f'§§PUBLISHER', str(publisher))
+
+        themes_string = self._join_list_to_string(themes_list, seperator=">, <", prefix="<", suffix=">")
+        dataset = dataset.replace('§§THEMES', themes_string)
+
+        for key, value in kwargs.items():
+            if key in ['ISSUED','MODIFIED']:
+                dataset = dataset.replace(f'§§{key}', self.dateTimeToXSDString(value))
+                continue
+            if key == "KEYWORDS":
+                keywords_string = self._join_list_to_string(value,seperator="\", \"",prefix="\"",suffix="\"")
+                dataset = dataset.replace(f'§§{key}', keywords_string)
+                continue
+            dataset = dataset.replace(f'§§{key}',str(value))
+
+        result_=""
+        for line in dataset.split('\n'):
+            if line.find('§§')>0:
+                continue
+            result_=f'{result_}\n{line}'
+        result_=result_.strip()
+        return result_
+
+    def createDistributionRDF(self, title, datasetid, version, publisher, mediatype, **kwargs):
+        dataset = self.distribution_template
+        dataset = dataset.replace(f'§§TITLE', str(title))
+        dataset = dataset.replace(f'§§DATASETID', str(datasetid))
+        dataset = dataset.replace(f'§§VERSION', str(version))
+        dataset = dataset.replace(f'§§PUBLISHER', str(publisher))
+        dataset = dataset.replace(f'§§MEDIATYPE', str(mediatype))
+
+
+        for key, value in kwargs.items():
+            if key in ['ISSUED','MODIFIED']:
+                dataset = dataset.replace(f'§§{key}', self.dateTimeToXSDString(value))
+                continue
+            dataset = dataset.replace(f'§§{key}',str(value))
+
+        result_=""
+        for line in dataset.split('\n'):
+            if line.find('§§')>0:
+                continue
+            result_=f'{result_}\n{line}'
+        result_=result_.strip()
+        return result_
+
+    def dateTimeToXSDString(self,date_obj):
+        if isinstance(date_obj,datetime.datetime):
+            return f'"{date_obj.isoformat()}"^^xsd:dateTime'
+        return date_obj
+
+    def _join_list_to_string(self, elements,seperator, prefix,suffix):
+        if isinstance(elements, list):
+            ret_string = seperator.join(elements)
+            ret_string = f'{prefix}{ret_string}{suffix}'
+            return ret_string
+
+        return str(elements)
+
 
 
